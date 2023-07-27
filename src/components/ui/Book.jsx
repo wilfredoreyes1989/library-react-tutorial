d75722ad59aed7029b2e1808cde751518f7ee841
Wilfredo <reyes7964@yahoo.com>
2023-07-27 16:22:14 -0400

    updated commit

---
+++ src/components/ui/Book.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+import Price from "./Price";
+
+const Book = ({ book }) => {
+  return (
+    <div className="book">
+      <Link to={`/books/${book.id}`} >
+        <figure className="book__img--wrapper">
+          <img src={book.url} alt="" className="book__img" />
+        </figure>
+      </Link>
+      <div className="book__title">
+        <Link to={`/books/${book.id}`} className="book__title--link">
+          {book.title}
+        </Link>
+      </div>
+      <Rating rating={book.rating}/>   
+      <Price salePrice={book.salePrice} originalPrice={book.originalPrice} /> 
+    </div>
+  );
+};
+
+export default Book;
---
+++ src/components/ui/Highlight.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+ const Highlight = ({ icon, title, para }) => {
+  return (
+    <div className="highlight">
+        <div className="highlight__img">
+            {icon}
+        </div>
+      <h3 className="hightlight__subtitle">{title}</h3>
+        <p className="hightlight para">
+            {para}
+        </p>
+    </div>
+  );
+}
+
+export default Highlight;
